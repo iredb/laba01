9b430277c5c05de5439c45a5bb0e2288e9e40cb3
iredb <120615742+iredb@users.noreply.github.com>
2023-09-27 16:59:17 +0700

    идеальный код

--- laba01.cpp
+++ laba01.cpp
@@ -10,27 +10,37 @@
 
 using namespace std;
 
-int main(){
-  double P2, Epsilon, A;
-
-  double T1 = 320;
-  double P1 = 0.981 * pow(10, 5);
-  double T2 = 973;
-  double n = 1.36;
+int main() {
   const double R = 8.3144;
+  double P2, epsilon, A, T1, P1, T2, n;
+
+  /*T1 = 320;
+  P1 = 0.981 * pow(10, 5);
+  T2 = 973;
+  n = 1.36;*/
+  cout << "T1 = ";
+  cin >> T1;
+  cout << endl;
+  cout << "P1 = ";
+  cin >> P1;
+  cout << endl;
+  cout "T2 = ";
+  cin >> T2;
+  cout << endl;
+  cout << "n = ";
+  cin >> n;
+  cout << endl;
   
   P2 = P1 * pow( (T2 / T1), (n / (n - 1) ) );
-  Epsilon = pow( (T2 / T1), (1 / (n - 1) ) );
-  A =( (R * T1) / (n - 1) ) * ( (T2 / T1) - 1 );
+  epsilon = pow( (T2 / T1), (1 / (n - 1) ) );
+  A = ( (R * T1) / (n - 1) ) * ( (T2 / T1) - 1 );
   
   cout.precision(6);
   cout << fixed;
   
-  cout << P2 << endl;
-  cout << Epsilon << endl;
-  cout << A << endl;
-
-  cout << "Что-то изменилось!";
+  cout << "P2 = " << P2 << endl 
+       << "epsilon = " << epsilon << endl
+       << "A = " << A << endl;
 
   return 0;
 }
